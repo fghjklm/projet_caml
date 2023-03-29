0becc4880262bb4b3bec8081f18dca7d236abb09
Mouysset Martin <martin.mouysset@gmail.com>
2023-03-29 20:10:18 +0200

    Update instrs.ml

--- postscript/Web/instrs.ml
+++ postscript/Web/instrs.ml
@@ -12,4 +12,14 @@ type instr =
 | ILoop of instr
 | IDef of fname * instr
 
-let string_of_instr instr = ""
+let rec string_of_instr instr = match instr with 
+	Ival val -> (match val with
+				BootT b -> Bool.to_string(b)
+				|FloatV f -> string_of_float(f)
+				|IntV i-> string_of_int (i)
+				|LitV l -> l
+				StringV s -> s)
+	|IVar i -> string_of_int(i)
+	|IOper op -> op
+	|IBloc instr -> string_of_instr instr
+	|ISeq l -> 
